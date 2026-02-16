7577c3f01800f1804491abc3a953af88f033daab
Piyush kumar raj <piyushobroy87@gmail.com>
2026-02-16 22:31:09 +0530

    Prefix sum Understanding

---
+++ prefix_sum/pivotElement.java
@@ -0,0 +1,28 @@
+package prefix_sum;
+
+public class pivotElement {
+    public static void main(String[] args) {
+        int[] nums = {1,7,3,6,5,6};
+        int res = pivotIndex(nums);
+        System.out.println(res);
+    }
+    static int pivotIndex(int[] nums){
+        int[] left = new int[nums.length];
+        left[0] = nums[0];
+        int[] right = new int[nums.length];
+        right[nums.length-1] = nums[nums.length-1];
+        for(int i=1;i<nums.length;i++){
+            left[i] = left[i-1]+nums[i];
+        }
+        for(int i=nums.length-2;i>=0;i--){
+            right[i] = right[i+1]+nums[i];
+        }
+
+        for(int i=0;i<nums.length;i++){
+            if(left[i]==right[i]){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
